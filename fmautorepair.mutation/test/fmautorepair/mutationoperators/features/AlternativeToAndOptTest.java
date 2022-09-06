b2177ae61f8192db73607073797007bd32d25637
Angelo Gargantini <angelo.gargantini@unibg.it>
2022-09-06 14:58:11 +0200

    fixed some tests

--- fmautorepair.mutation/test/fmautorepair/mutationoperators/features/AlternativeToAndOptTest.java
+++ fmautorepair.mutation/test/fmautorepair/mutationoperators/features/AlternativeToAndOptTest.java
@@ -1,7 +1,6 @@
 package fmautorepair.mutationoperators.features;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
 
 import java.io.FileNotFoundException;
